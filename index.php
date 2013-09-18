a4f535e6404b39351c9185630716fe5b466c6232
Rachel Boyce <rfboyce@gmail.com>
2013-09-17 22:26:05 -0400

    added content and updated css

--- index.php
+++ index.php
@@ -2,8 +2,16 @@
 <head>
 <title>a page</title>
 <link href="css/style.css" rel="stylesheet" type="text/css" />
+<h1>Welcome to P1</h1>
 </head>
 <body>
-<?php echo "Welcome to P1"; ?>
+<h2>Hello. My name is Rachel Boyce.</h2>
+<p>I'm a data and project manager at the Harvard Center for Population and
+Development Studies. I occasionally take on freelance WordPress
+projects, so I am excited to learn more about PHP. I really enjoyed learning Processing last
+semester, it appealed to my interests in creative computing and new
+media art. I am a painter and video artist, DJ, and bicyclist.</p>
+<p>My favorite text editor is Aquamacs, which is what I'm using to
+write this. </p>
 </body>
 </html>
